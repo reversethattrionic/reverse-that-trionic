322e54c664f08771c4c5913b6d42e4ff46b3a17e
patrik.servin <patrik.servin@256b9357-cf33-0410-adab-d11efc7c0b40>
2007-09-22 21:44:47 +0000

    made a copy

---
+++ tags/Release_1.0.0.4/KWP/KWPHandler.cs
@@ -0,0 +1,238 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T7Tool.KWP
+{
+    enum KWPResult
+    {
+        OK,
+        Timeout,
+        DeviceNotConnected
+    }
+
+    class KWPHandler
+    {
+        IKWPDevice m_kwpDevice;
+
+        public KWPHandler(IKWPDevice a_kwpDevice)
+        {
+            m_kwpDevice = a_kwpDevice;
+        }
+
+        public KWPHandler()
+        {
+        }
+
+        public void setKWPDevice(IKWPDevice a_device)
+        {
+            m_kwpDevice = a_device;
+        }
+
+        public bool startSession()
+        {
+            return m_kwpDevice.startSession();
+        }
+
+        public bool openDevice()
+        {
+            return m_kwpDevice.open();
+        }
+
+        public bool closeDevice()
+        {
+            return m_kwpDevice.close();
+        }
+
+        /// <summary>
+        /// Send a request for sequrity access.
+        /// This is needed to access protected functions (flash reading and writing).
+        /// </summary>
+        /// <returns>True on success, otherwise false.</returns>
+        public bool requestSequrityAccess()
+        {
+            //Try method 1
+            if (requestSequrityAccessLevel(1))
+                return true;
+            if (requestSequrityAccessLevel(2))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Send a request for a sequrity access with one out of two methods to 
+        /// calculate the key.
+        /// </summary>
+        /// <param name="a_method">Key calculation method [1,2]</param>
+        /// <returns>true if sequrity access was granted, otherwise false</returns>
+        private bool requestSequrityAccessLevel(uint a_method)
+        {
+            KWPReply reply = new KWPReply();
+            KWPResult result;
+            byte[] seed = new byte[2];
+            byte[] key = new byte[2];
+            // Send a seed request.
+            result = sendRequest(new KWPRequest(0x27, 0x05), out reply);
+            if (result != KWPResult.OK)
+                return false;
+            seed[0] = reply.getData()[0];
+            seed[1] = reply.getData()[1];
+            if (a_method == 1)
+                key = calculateKey(seed, 0);
+            else
+                key = calculateKey(seed, 1);
+            // Send key reply.
+            result = sendRequest(new KWPRequest(0x27, 0x06, key), out reply);
+            if (result != KWPResult.OK)
+                return false;
+            //Check if sequrity was granted.
+            if ((reply.getMode() == 0x67) && (reply.getData()[0] == 0x34))
+                return true;
+
+            return false;
+        }
+
+        public KWPResult getVIN(out string r_vin)
+        {
+            KWPReply reply = new KWPReply();
+            KWPResult result;
+            result = sendRequest(new KWPRequest(0x1A,0x90), out reply);
+            if (result == KWPResult.OK)
+            {
+                r_vin = getString(reply);
+                return KWPResult.OK;
+            }
+            else
+            {
+                r_vin = "";
+                return KWPResult.Timeout;
+            }
+        }
+
+        public KWPResult getImmo(out string r_immo)
+        {
+            KWPReply reply = new KWPReply();
+            KWPResult result;
+            result = sendRequest(new KWPRequest(0x1A, 0x92), out reply);
+            r_immo = getString(reply);
+            return result;
+        }
+
+        public KWPResult getSwPartNumber(out string r_swPartNo)
+        {
+            KWPReply reply = new KWPReply();
+            KWPResult result;
+            result = sendRequest(new KWPRequest(0x1A, 0x94), out reply);
+            r_swPartNo = getString(reply);
+            return result;
+        }
+
+        public KWPResult getSwVersion(out string r_swVersion)
+        {
+            KWPReply reply = new KWPReply();
+            KWPResult result;
+            result = sendRequest(new KWPRequest(0x1A, 0x95), out reply);
+            r_swVersion = getString(reply);
+            return result;
+        }
+
+        public KWPResult getEngineType(out string r_swVersion)
+        {
+            KWPReply reply = new KWPReply();
+            KWPResult result;
+            result = sendRequest(new KWPRequest(0x1A, 0x97), out reply);
+            r_swVersion = getString(reply);
+            return result;
+        }
+
+        public bool sendReadRequest(uint a_address, uint a_length)
+        {
+            KWPReply reply = new KWPReply();
+            KWPResult result;
+            byte[] lengthAndAddress = new byte[5];
+            //set length (byte 0 and 1)
+            lengthAndAddress[0] = (byte)(a_length >> 8);
+            lengthAndAddress[1] = (byte)(a_length);
+            //set address (byte 2 to 4);
+            lengthAndAddress[2] = (byte)(a_address >> 16);
+            lengthAndAddress[3] = (byte)(a_address >> 8);
+            lengthAndAddress[4] = (byte)(a_address);
+            result = sendRequest(new KWPRequest(0x2C, 0xF0, 0x03, lengthAndAddress), out reply);
+            if (result == KWPResult.OK)
+                return true;
+            else
+                return false;
+        }
+
+        public bool sendDataTransferExitRequest()
+        {
+            KWPReply reply = new KWPReply();
+            KWPResult result;
+            result = sendRequest(new KWPRequest(0x82, 0x00), out reply);
+            if (result == KWPResult.OK)
+                return true;
+            else 
+                return false;
+        }
+
+        public bool sendDataTransferRequest(out byte[] r_data)
+        {
+            KWPReply reply = new KWPReply();
+            KWPResult result;
+            result = sendRequest(new KWPRequest(0x21, 0xF0), out reply);
+            r_data = reply.getData();
+            if (result == KWPResult.OK)
+                return true;
+            else 
+                return false;
+        }
+
+        private KWPResult sendRequest(KWPRequest a_request, out KWPReply a_reply)
+        {
+            KWPReply reply = new KWPReply();
+            RequestResult result;
+            a_reply = new KWPReply();
+            if (!m_kwpDevice.isOpen())
+                return KWPResult.DeviceNotConnected;
+            result = m_kwpDevice.sendRequest(a_request, out reply);
+            a_reply = reply;
+            if (result == RequestResult.NoError)
+                return KWPResult.OK;
+            else
+                return KWPResult.Timeout;
+        }
+
+        private string getString(KWPReply a_reply)
+        {
+            Encoding ascii = Encoding.ASCII;
+            ascii.GetChars(a_reply.getData(), 0, a_reply.getData().Length);
+            return ascii.GetString(a_reply.getData(), 0, a_reply.getData().Length);
+        }
+
+        /// <summary>
+        /// Calculate key for a seed.
+        /// </summary>
+        /// <param name="a_seed">Byte array with two bytes representing the seed.</param>
+        /// <param name="a_method">Type of method to use for calculation [0,1].</param>
+        /// <returns>Byte array with two bytes representing the key.</returns>
+        private byte[] calculateKey(byte[] a_seed, uint a_method)
+        {
+            int key;
+            byte[] returnKey = new byte[2];
+            int seed = a_seed[0] << 8 | a_seed[1];
+    
+            key = seed << 2;
+            key &= 0xFFFF;
+            key ^= (a_method == 1 ? 0x4081 : 0x8142);
+            key -= (a_method == 1 ? 0x1F6F : 0x2356);
+            key &= 0xFFFF;
+
+            returnKey[0] = (byte)((key >> 8) & 0xFF);
+            returnKey[1] = (byte)(key & 0xFF);
+
+            return returnKey;
+        }
+
+    }
+}
---
+++ tags/Release_1.0.0.4/KWP/KWPReply.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T7Tool.KWP
+{
+    class KWPReply
+    {
+        byte[] m_reply;
+        uint m_nrOfPid;
+
+        public KWPReply(byte[] a_reply, uint a_nrOfPid)
+        {
+            if ((a_nrOfPid > 2) || (a_nrOfPid < 1))
+                throw new Exception("Nr of PID out of range");
+            m_reply = a_reply;
+            m_nrOfPid = a_nrOfPid;
+        }
+
+        public KWPReply()
+        {
+        }
+
+        public void setData(byte[] a_data)
+        {
+            m_reply = a_data;
+        }
+
+        public void setNrOfPID(uint a_nr) { m_nrOfPid = a_nr; }
+        public uint getNrOfPID() { return m_nrOfPid; }
+
+        public byte getLength()
+        {
+            return m_reply[0];
+        }
+
+        public byte getMode()
+        {
+            return m_reply[1];
+        }
+
+        public byte getPid()
+        {
+            return m_reply[2];
+        }
+
+        public byte getPidHigh()
+        {
+            return m_reply[2];
+        }
+
+        public byte getPidLow()
+        {
+            return m_reply[3];
+        }
+
+        public byte[] getData()
+        {
+            byte[] data = new byte[getLength() - m_nrOfPid - 1];
+            uint i;
+            if (m_nrOfPid == 1)
+                i = 3;
+            else
+                i = 4;
+            for (uint j = 0; j < data.Length; i++, j++)
+                data[j] = m_reply[i];
+            return data;
+        }
+    }
+}
