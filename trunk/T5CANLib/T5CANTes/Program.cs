eb21fab0c6644747648dd9f5c934e0d409ba3234
patrik.servin <patrik.servin@256b9357-cf33-0410-adab-d11efc7c0b40>
2008-05-28 21:15:41 +0000

    Random access reading from RAM works now.

--- trunk/T5CANLib/T5CANTes/Program.cs
+++ trunk/T5CANLib/T5CANTes/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Text;
 using T5CANLib.CAN;
@@ -24,13 +25,23 @@ namespace T5CANTest
             swVersion = t5can.getSWVersion();
             System.Console.WriteLine("SW version: " + swVersion);
             System.Console.WriteLine();
-            //symbolTable =t5can.getSymbolTable();
-            //System.Console.WriteLine("Symbol table: " + symbolTable);
-            //System.Console.WriteLine();
+            System.Console.WriteLine("Fetching symbol table...");
+            symbolTable =t5can.getSymbolTable();
+            System.Console.WriteLine("Symbol table: " + symbolTable);
+            System.Console.WriteLine();
 
-            byte[] ram = t5can.readRAM(0x0, 32);
-            System.Console.WriteLine("RAM: ");
-            for (int i = 0; i < ram.Length; i++)
+            UInt16 length = 32 * 1024;
+            string fileName = "dumpram.bin";
+            System.Console.WriteLine("Dumping 32 kB of RAM to dumpram.bin: ");
+            byte[] ram = t5can.readRAM(0x0000, length);
+            
+
+            if (File.Exists(fileName))
+                File.Delete(fileName);
+            FileStream fileStream = File.Create(fileName, length);
+            fileStream.Write(ram, 0, length);
+            fileStream.Close();
+           /* for (int i = 0; i < ram.Length; i++)
             {
                 if (i % 16 == 0)
                     System.Console.WriteLine();
@@ -39,7 +50,7 @@ namespace T5CANTest
                     System.Console.Write("0");
                 System.Console.Write(str + " ");
                 
-            }
+            }*/
             Environment.Exit(0);
         }
     }
